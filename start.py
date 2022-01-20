f9625ecbc09fdc2c9318fea5751b335694b292fa
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-01-20 21:02:26 +0330

    this is a solid version

--- start.py
+++ start.py
@@ -1,16 +1,19 @@
-# import librar's 
+# import librar's
 import os
 from rand_string.rand_string import RandString
-from colorama import Fore,init
+from colorama import Fore, init
 
 # start coding folder bomber main
 print(" 😈 Let's Go . . .")
 
 count = int(input(Fore.YELLOW+" [?] "+Fore.WHITE+"How mony time's ? :  "))
+# define lenght for directory name's
+lenght = int(input(Fore.YELLOW+" [?] "+Fore.WHITE+"lenght ? : "))
 
 for i in range(0, count):
-    lenght = int(input(Fore.YELLOW+" [?] "+Fore.WHITE+"lenght ? : "))
+    # making directory
     os.mkdir(RandString("uppercase", lenght))
-    print(Fore.RED+" [$] "+Fore.WHITE+"Making Folder :: ",i,"\n")
+    # print Result
+    print(Fore.RED+" [$] "+Fore.WHITE+"Making Folder :: ", i, "\n")
 
 print(" 🩸 End . . .")
