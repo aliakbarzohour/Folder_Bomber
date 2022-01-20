c49eeef9e93c7bf57b24a17b63469651792f9c57
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-01-20 21:08:23 +0330

    comenting ...

--- start.py
+++ start.py
@@ -6,10 +6,14 @@ from colorama import Fore, init
 # start coding folder bomber main
 print(" 😈 Let's Go . . .")
 
+# define counter for making directory
 count = int(input(Fore.YELLOW+" [?] "+Fore.WHITE+"How mony time's ? :  "))
 # define lenght for directory name's
 lenght = int(input(Fore.YELLOW+" [?] "+Fore.WHITE+"lenght ? : "))
-
+# -------------------------------------------------------------------------
+# I'm building this loop to both have better control over the construction 
+# of the routes and to be able to reduce my code.
+# -------------------------------------------------------------------------
 for i in range(0, count):
     # making directory
     os.mkdir(RandString("uppercase", lenght))
