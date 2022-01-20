fc769581341f9df6e75eb3242b1daa6618078f70
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-01-20 20:48:22 +0330

    debuging .

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 # import librar's 
 import os
-from rand_string import RandomString
+from rand_string.rand_string import RandString
 from colorama import Fore,init
 
 # start coding folder bomber main
 print(" 😈 Let's Go . . .")
 
 count = int(input(Fore.YELLOW+" [?] "+Fore.WHITE+"How mony time's ? :  "))
+
+for i in range(0, count):
+    print("hello", i)
 
 print(" 🩸 End . . .")
